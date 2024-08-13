0436a6a6c0c179df574df814b1fffd2b3f75ff65
Mohamed_ElhajAbdou <deathorglory.2017@gmail.com>
2024-08-13 20:02:59 +0400

    added the origin

--- app.js
+++ app.js
@@ -76,6 +76,47 @@
 //   console.log(`Server running on port ${port}`);
 // });
 /////////////////////////////////////////////////////////////
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors'); // Include CORS middleware
+// const { connectDB } = require('./config/mongoConfig');
+// const queryRoutes = require('./routes/query');
+// const authRoutes = require('./routes/authRoutes');
+// const insertDataRoute = require('./routes/insertDataRoute');
+// const aggregateRoutes = require('./routes/aggregate');
+
+// const app = express();
+
+// // Middleware to parse JSON bodies
+// app.use(bodyParser.json());
+
+// // Configure CORS to allow requests from your frontend
+// app.use(cors({
+//   origin: 'http://localhost:5173'|| 'https://social-media-ruby-phi.vercel.app', // Allow requests from the frontend
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify allowed methods
+//   allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+// }));
+
+// // Connect to the database
+// connectDB();
+
+// // Routes
+// app.use('/api', queryRoutes);
+// app.use('/api', authRoutes);
+// app.use('/api', aggregateRoutes);
+// app.use('/api', insertDataRoute);
+
+// // Start the server
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
+
+
+
+////////////////////////////////////////////////////
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors'); // Include CORS middleware
@@ -92,7 +133,10 @@ app.use(bodyParser.json());
 
 // Configure CORS to allow requests from your frontend
 app.use(cors({
-  origin: 'http://localhost:5173'|| 'https://social-media-ruby-phi.vercel.app', // Allow requests from the frontend
+  origin: [
+    'http://localhost:5173',
+    'https://social-media-ruby-phi.vercel.app'
+  ], // Allow requests from the specified frontends
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify allowed methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
 }));
