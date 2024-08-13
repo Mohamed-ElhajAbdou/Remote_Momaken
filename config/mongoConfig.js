d9ca98f1fdd6b173f0802acce29a899482972672
Mohamed_ElhajAbdou <deathorglory.2017@gmail.com>
2024-08-13 21:06:52 +0400

    added the origin

--- config/mongoConfig.js
+++ config/mongoConfig.js
@@ -4,8 +4,15 @@ const { MongoClient } = require('mongodb');
 const connectDB = async () => {
   const uri = 'mongodb+srv://Mohamed:Test%4012345@cluster0.u4juk.mongodb.net/myDatabase?retryWrites=true&w=majority&appName=Cluster0';
 
-  const client = new MongoClient(uri);
+  // const client = new MongoClient(uri);
 
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
+    socketTimeoutMS: 45000,          // Increase socket timeout to 45 seconds
+  });
+  
   try {
     await client.connect();
     console.log('Connected to MongoDB');
